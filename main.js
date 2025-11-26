4b0bc654ff80a946a59dedcad2b11d6084673317
Clearviewneeded <jsjr2409@gmail.com>
2025-11-26 23:03:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let age = 20;
+function displayAge() {
+  console.log("Current age:", age);
+}
+
+function changeAge(newAge) {
+  age = newAge;
+  console.log("Age after updating:", age);
+}
+displayAge();      
+changeAge(25);   
+displayAge();    
